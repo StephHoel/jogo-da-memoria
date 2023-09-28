731e740ca824b5b08a4526e2fdf4c516b7d3b4a2
Steph Hoel <steph.hoel@gmail.com>
2023-09-28 09:58:09 -0300

    feat: memory game

---
+++ src/scripts/engine.js
@@ -0,0 +1,57 @@
+const emojis = [
+  "💵",
+  "💵",
+  "😱",
+  "😱",
+  "❤️",
+  "❤️",
+  "😆",
+  "😆",
+  "😍",
+  "😍",
+  "🚀",
+  "🚀",
+  "❌",
+  "❌",
+  "🚧",
+  "🚧"
+]
+let openCards = []
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1))
+
+for (let i = 0; i < emojis.length; i++){
+  let box = document.createElement("div")
+  box.className="item"
+  box.innerHTML = shuffleEmojis[i]
+  box.onclick=handleClick
+  
+  document.querySelector(".game").appendChild(box)
+}
+
+function handleClick() {
+  if (openCards.length < 2) {
+    this.classList.add('boxOpen')
+    openCards.push(this)
+  }
+
+  if (openCards.length == 2) {
+    setTimeout(checkMatch, 300)
+  }
+}
+
+function checkMatch() {
+  if (openCards[0].innerHTML === openCards[1].innerHTML) {
+    openCards[0].classList.add('boxMatch')
+    openCards[1].classList.add('boxMatch')
+  } else {
+    openCards[0].classList.remove('boxOpen')
+    openCards[1].classList.remove('boxOpen')
+  }
+
+  openCards = []
+
+  if (document.querySelectorAll('.boxMatch').length === emojis.length) {
+    
+  }
+}
